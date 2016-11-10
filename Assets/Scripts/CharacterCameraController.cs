1c9fa8b58e5a51d2f2781cf93a969dc5eae3c45b
Alexander Khomchenko <alexandr.khomchenko@gmail.com>
2016-11-10 17:08:06 +0200

    simplify code

---
+++ Assets/Scripts/CharacterCameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterCameraController : MonoBehaviour {
+
+    Vector2 lookAngle;
+    public float sensitivity = 2.0f;
+
+    public GameObject character;
+
+	void Start () {
+	   character = this.transform.parent.gameObject;
+	}
+	
+	void Update () {
+	   var md = new Vector2(Input.GetAxisRaw("Mouse X"), 
+                            Input.GetAxisRaw("Mouse Y"));
+       md *= sensitivity;
+       lookAngle += md;
+
+       transform.localRotation = Quaternion.AngleAxis(-lookAngle.y, Vector3.right);
+       character.transform.localRotation = Quaternion.AngleAxis(lookAngle.x, character.transform.up);
+	}
+}
---
+++ Assets/Scripts/CharacterFirstPersonController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterFirstPersonController : MonoBehaviour {
+
+    public float speed = 10.0F;
+
+	void Start () {
+	   Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+	}
+
+    void FixedUpdate () {
+        moveCharacter();        
+    }
+
+    void moveCharacter () {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float straffe = Input.GetAxis("Horizontal") * speed;
+    
+        transform.Translate(straffe, 0, translation);
+    }
+}
