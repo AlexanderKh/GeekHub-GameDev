dff6d262e31130c962af805b9ab6b3d6fbd6d8e9
Alexander Khomchenko <alexandr.khomchenko@gmail.com>
2016-12-02 18:29:56 +0200

    implement pin respawn

--- Assets/Scene 3/BowlingCharacterController.cs
+++ Assets/Scene 3/BowlingCharacterController.cs
@@ -5,12 +5,20 @@ public class BowlingCharacterController : MonoBehaviour {
 
     public Transform prefab;
 
+    public Transform originalKegli;
+    public Transform prefabKegli;
+
     private Transform self;
     private Transform camera;
+
+    private Vector3 originalKegliPosition;
+    private Quaternion originalKegliRotation;
 
     void Start () {
         self = transform;
         camera = GetComponentInChildren<Transform> ();
+        originalKegliPosition = originalKegli.position;
+        originalKegliRotation = originalKegli.rotation;
     }
 
 	void Update () {
@@ -18,6 +26,10 @@ public class BowlingCharacterController : MonoBehaviour {
             var o = Instantiate (prefab, self.position + camera.forward, Quaternion.identity) as Transform;
             var rb = o.GetComponent<Rigidbody> ();
             rb.AddForce (camera.forward * 500);
+        }
+        if (Input.GetButtonDown("Fire3")) {
+            Destroy (originalKegli.gameObject);
+            originalKegli = Instantiate (prefabKegli, originalKegliPosition, originalKegliRotation) as Transform;
         }
 	}
 }
