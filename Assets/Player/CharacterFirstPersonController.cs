5bebc2b7f12808a73dfdf3014888c3c18734eefc
Alexander Khomchenko <alexandr.khomchenko@gmail.com>
2016-12-02 15:43:48 +0200

    implement ball throwing

--- Assets/Player/CharacterFirstPersonController.cs
+++ Assets/Player/CharacterFirstPersonController.cs
@@ -6,12 +6,10 @@ public class CharacterFirstPersonController : MonoBehaviour {
     public float speed = 10.0F;
 
     private GameObject cam;
-    private Vector3 v;
 
 	void Start () {
         cam = GetComponentInChildren<Camera> ().gameObject;
 	    Cursor.lockState = CursorLockMode.Locked;
-        v = cam.transform.forward;
 	}
 	
 	void Update () {
@@ -19,7 +17,6 @@ public class CharacterFirstPersonController : MonoBehaviour {
             Cursor.lockState = CursorLockMode.None;
         if (Input.GetButtonDown("Fire1")) {
             interact ();
-            v = cam.transform.forward;
         }
 	}
 
@@ -37,8 +34,6 @@ public class CharacterFirstPersonController : MonoBehaviour {
 
     void interact() {
         RaycastHit hit;
-
-
         if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, 2)) {
             GameObject go = hit.collider.gameObject;
             if (go.CompareTag("Interactive")) {
--- Assets/Scene 3/BowlingCharacterController.cs
+++ Assets/Scene 3/BowlingCharacterController.cs
@@ -5,9 +5,19 @@ public class BowlingCharacterController : MonoBehaviour {
 
     public Transform prefab;
 
+    private Transform self;
+    private Transform camera;
+
+    void Start () {
+        self = transform;
+        camera = GetComponentInChildren<Transform> ();
+    }
+
 	void Update () {
         if (Input.GetButtonDown("Fire2")) {
-            Instantiate (prefab, Vector3.zero, Quaternion.identity);
+            var o = Instantiate (prefab, self.position + camera.forward, Quaternion.identity) as Transform;
+            var rb = o.GetComponent<Rigidbody> ();
+            rb.AddForce (camera.forward * 400);
         }
 	}
 }
