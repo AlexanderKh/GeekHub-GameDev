2349256fff780df60186bb1bb691e730e12d3f06
Alexander Khomchenko <alexandr.khomchenko@gmail.com>
2016-12-09 00:20:49 +0200

    add closest painting text

---
+++ Assets/Scene 4/ClosestPaintingLabelController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ClosestPaintingLabelController : MonoBehaviour
+{
+    private GameObject[] paintings;
+    private GameObject player;
+
+    private Text text;
+
+    void Start ()
+    {
+        paintings = GameObject.FindGameObjectsWithTag ("Painting");
+        player = GameObject.FindGameObjectWithTag ("Player");
+        text = GetComponent<Text> ();
+    }
+
+    void Update ()
+    {
+        if (paintings.GetLength (0) == 0) {
+            print ("bye");
+            return;
+        }
+        GameObject closest = paintings [0];
+        float closestDistance = GetDistance (closest, player);
+        foreach (var painting in paintings) {
+            float distance = GetDistance (painting, player);
+            if (distance < closestDistance) {
+                closest = painting;
+                closestDistance = distance;
+            }
+        }
+
+        text.text = closest.name;
+    }
+
+    float GetDistance (GameObject a, GameObject b)
+    {
+        var cuurentPaintingPosition = a.GetComponent<Transform> ().position;
+        var lastClosestPaintingPosition = b.GetComponent<Transform> ().position;
+        return Vector3.Distance (cuurentPaintingPosition, lastClosestPaintingPosition);
+    }
+}
