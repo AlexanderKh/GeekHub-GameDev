454c23adcb6b2a3bc6eade7ee8650d90b93be588
Alexander Khomchenko <alexandr.khomchenko@gmail.com>
2016-12-20 23:23:41 +0200

    add animation to show more button

---
+++ Assets/Scene 4/ShowMore Button/ShowMoreButtonController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowMoreButtonController : MonoBehaviour
+{
+    private Animator animator;
+
+    public void Start () 
+    {
+        animator = GetComponent<Animator> ();
+    }
+
+    public void Highlighted()
+    {
+        print("hh");
+        animator.SetBool ("Highlighted", true);
+    }
+
+    public void Unhighlighted()
+    {
+        print("uuhh");
+        animator.SetBool ("Highlighted", false);
+    }
+}
