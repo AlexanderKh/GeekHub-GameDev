bbbc9dfa07fea068de1dec5e946263862d7297e3
Alexander Khomchenko <alexandr.khomchenko@gmail.com>
2016-12-21 12:34:18 +0200

    funny

---
+++ Assets/Scene 3/PinCollider.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PinCollider : MonoBehaviour
+{
+
+    public Text text;
+
+    void Start ()
+    {
+	    
+    }
+
+    void Update ()
+    {
+	
+    }
+
+    void OnCollisionEnter (Collision col)
+    {
+        if (col.collider.isTrigger) {
+            if (col.gameObject.name == "Road") {
+                text.text = "falllen";
+            }
+        }
+    }
+}
