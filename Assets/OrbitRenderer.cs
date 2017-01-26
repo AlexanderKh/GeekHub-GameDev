dd0104ed394419622dfb8c63381937261663ca62
Alexander Khomchenko <alexandr.khomchenko@gmail.com>
2017-01-26 21:21:24 +0200

    add one planet on orbit

---
+++ Assets/OrbitRenderer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrbitRenderer : MonoBehaviour
+{
+
+    public Material material;
+    public int points = 100;
+    public float width = 0.2f;
+    public float radius = 3f;
+
+    private LineRenderer lr;
+
+    void Start ()
+    {
+        lr = gameObject.AddComponent<LineRenderer>();
+        lr.material = material;
+        lr.SetWidth (width, width);
+        lr.SetVertexCount (points + 1);
+
+        float fullCircle = 2 * Mathf.PI;
+        float circlePartial = fullCircle / points;
+
+        for(int i = 0; i <= points; i++) {
+            float x, y;
+            GetCartesian (radius, circlePartial * i, out x, out y);
+            Vector3 position = new Vector3 (x, 0, y);
+            lr.SetPosition(i, position);
+        }
+    }
+
+    private void GetCartesian (float r, float theta, out float x, out float y)
+    {
+        x = r * Mathf.Cos (theta);
+        y = r * Mathf.Sin (theta);
+    }
+}
---
+++ Assets/PlanetSpin.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetSpin : MonoBehaviour
+{
+
+    public float speed = 1f;
+
+    void Start ()
+    {
+	
+    }
+	
+    void Update ()
+    {
+        Quaternion rotation = transform.rotation;
+        Vector3 angles = rotation.eulerAngles;
+        angles.y += speed;
+        rotation.eulerAngles = angles;
+        transform.rotation = rotation;
+    }
+}
