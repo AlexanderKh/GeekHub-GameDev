a620b71185d0c65b1e0c9292343bb7d21398cf9b
Alexander Khomchenko <alexandr.khomchenko@gmail.com>
2017-01-30 01:05:44 +0200

    implement planet selection

--- Assets/PlanetController.cs
+++ Assets/PlanetController.cs
@@ -4,35 +4,66 @@ using System.Collections;
 public class PlanetController : MonoBehaviour
 {
     public string name = "Earth";
+    public float speed = 1f;
 
+    private GameObject sun;
     private PlanetInfoTextController planetInfoTextCtrl;
     private int axisRotations;
     private int orbitRotations;
+    private float angle;
+    private int sign;
+    private Behaviour halo;
 
     void Start ()
     {
         GameObject planetInfoText = GameObject.FindGameObjectWithTag("Planet Info Text");
         planetInfoTextCtrl = planetInfoText.GetComponent<PlanetInfoTextController> ();
+        sun = GameObject.FindGameObjectWithTag ("Sun");
         axisRotations = 0;
         orbitRotations = 0;
+        angle = 0f;
+        sign = (speed >= 0) ? 1 : -1;
+        halo = (Behaviour) GetComponent ("Halo");
+        DisableHalo ();
+    }
+
+    void Update ()
+    {
+        transform.RotateAround (sun.transform.position, Vector3.up, speed);
+        angle += speed;
+        if ((angle * sign) > 360f) {
+            IncrementOrbitRotations ();
+            angle -= 360 * sign;
+        }
     }
 	
     void OnMouseDown ()
     {
         planetInfoTextCtrl.SelectPlanet (name);
         UpdatePlanetText ();
+        EnableHalo ();
     }
 
-    void IncrementAxisRotations ()
+    public void IncrementAxisRotations ()
     {
         axisRotations++;
         UpdatePlanetText ();
     }
 
-    void IncrementOrbitRotations ()
+    public void IncrementOrbitRotations ()
     {
         orbitRotations++;
         UpdatePlanetText ();
+    }
+
+    public void EnableHalo ()
+    {
+        halo.enabled = true;
+    }
+
+    public void DisableHalo ()
+    {
+        halo.enabled = false;
     }
 
     private void UpdatePlanetText ()
