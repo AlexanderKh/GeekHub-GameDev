f6c1bea903c90ef0ed986d22a7ad21a32f0c5662
Alexander Khomchenko <alexandr.khomchenko@gmail.com>
2017-01-30 22:48:51 +0200

    add other paintings

--- Assets/Scene 4/PaintingsData.cs
+++ Assets/Scene 4/PaintingsData.cs
@@ -13,6 +13,26 @@ public class PaintingsData : MonoBehaviour
             "Alex Khomchenko",
             "I took this photo at the CherITy fest 2015"
         ));
+        data.Add ("Mortal Combat", new Painting(
+            "Mortal Combat",
+            "TheSyanArt",
+            "Mortal Kombat Scorpion Wallpaper"
+        ));
+        data.Add ("Nikola Tesla", new Painting(
+            "Nikola Tesla",
+            "vinartvin",
+            "Nikola Tesla Art"
+        ));
+        data.Add ("Beatles", new Painting(
+            "The Beatles",
+            "vinartvin",
+            "The Beatles Art"
+        ));
+        data.Add ("Speedpaint", new Painting(
+            "Speed Paint #75",
+            "Sylar113",
+            "http://sylar113.deviantart.com/art/speedpaint-75-584565460"
+        ));
     }
 
     public static Painting GetPaintingByKey (string key)
