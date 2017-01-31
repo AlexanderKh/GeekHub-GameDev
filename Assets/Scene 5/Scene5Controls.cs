f7f6396f1e90055b25e8c26e736c45b49afd570c
Alexander Khomchenko <alexandr.khomchenko@gmail.com>
2017-01-31 12:48:32 +0200

    add scene transitions

---
+++ Assets/Scene 5/Scene5Controls.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scene5Controls : MonoBehaviour
+{
+    public GameObject orbits;
+    public GameObject sunFlare;
+	
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.O)) {
+            orbits.SetActive (!orbits.activeSelf);
+        }
+        if (Input.GetKeyDown(KeyCode.F)) {
+            sunFlare.SetActive (!sunFlare.activeSelf);
+        }
+        if (Input.GetKeyDown(KeyCode.X)) {
+            Application.LoadLevel ("Scene 1");
+        }
+    }
+}
